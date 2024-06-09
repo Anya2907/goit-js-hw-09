a9a6e507d8316cd64f51a9c1684edb01753875cf
Anya2907 <67818359+Anya2907@users.noreply.github.com>
2024-06-09 19:53:45 +0300

    HW 9 task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,23 +1,18 @@
 const formData = {
-    email: " ",
-    message: " ",
+    email: "",
+    message: "",
 }
-// const formData = {
-//     email: "neplokh@ukr.net",
-//     message: "Hello",
-// }
 
 const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', onInput);
 // form.addEventListener('submit', onSubmit);
 
-function onInput(evt) {
-    formData.email = evt.currentTarget.elements.email.value;
-    formData.message = evt.currentTarget.elements.message.value;    
+function onInput(evt) {    
+    formData.email = form.elements.email.value;
+    formData.message= form.elements.message.value;
+     
 }
-
-
 
 console.log(formData);
 localStorage.setItem("feedback-form-state", JSON.stringify(formData));
