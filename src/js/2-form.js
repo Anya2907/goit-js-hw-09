1267b55bdbb1706808cef2692cd764e30113f3d0
Anya2907 <67818359+Anya2907@users.noreply.github.com>
2024-06-10 00:36:32 +0300

    hw 9 task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,13 +6,38 @@ const formData = {
 const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', onInput);
-// form.addEventListener('submit', onSubmit);
+form.addEventListener('submit', onSubmit);
 
 function onInput(evt) {    
     formData.email = form.elements.email.value;
-    formData.message= form.elements.message.value;
-     
+    formData.message = form.elements.message.value;
+    
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));    
 }
 
-console.log(formData);
-localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+const data = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+if (data) {
+    form.elements.email.value = data.email;
+    form.elements.message.value = data.message;
+} else {
+    form.elements.email.value = '';
+    form.elements.message.value = '';
+}
+
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    const email = evt.currentTarget.elements.email.value;
+    const message = evt.currentTarget.elements.message.value;
+
+    if (!(email && message)) {
+        alert("Fill please all fields");
+    } else {
+        console.log(formData);
+        localStorage.removeItem("feedback-form-state");
+        form.reset();
+    }
+}
+
+
